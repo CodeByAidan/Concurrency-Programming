c95a2921fcf534965be26a4be252fb810eab56a8
CodeByAidan <67598470+CodeByAidan@users.noreply.github.com>
2023-09-28 00:22:18 -0400

    Refactor computeFactorials method to computeFactorial.

--- src/main/java/org/codebyaidan/ch2/Factorials.java
+++ src/main/java/org/codebyaidan/ch2/Factorials.java
@@ -1,16 +1,33 @@
 package org.codebyaidan.ch2;
 
 class Factorials {
-    static int computeFactorials(int k) {
+
+    static int computeFactorial(int k) {
         if (k < 0) return -1;
         else if (k == 0 || k == 1) return 1;
-        else return k*computeFactorials(k-1);
+        else return k*computeFactorial(k-1);
     }
+
     public static void main(String[] args) {
         int n;
         n = Integer.parseInt(args[0]);
         for (int i = 1; i <= n; i++)
-            System.out.println
-                (i + " factorial is " + computeFactorials(i));
+            System.out.println(i + " factorial is " + computeFactorial(i));
     }
 }
+
+/* ............... Example compile and run(s)
+
+D:\>javac fact.java
+
+D:\>java Factorials 5
+1 factorial is 1
+2 factorial is 2
+3 factorial is 6
+4 factorial is 24
+5 factorial is 120
+
+D:\>java Factorials
+java.lang.ArrayIndexOutOfBoundsException: 0
+        at Factorials.main(fact.java:11)
+                                            ... end of example run(s)  */
