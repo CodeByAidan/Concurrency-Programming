77356d2df19372459b22d79985e4d51d881fd073
CodeByAidan <67598470+CodeByAidan@users.noreply.github.com>
2023-09-28 00:03:25 -0400

    Add Factorials.java, Main.java to the repository.

---
+++ src/main/java/org/codfebyaidan/ch2/Factorials.java
@@ -0,0 +1,16 @@
+package org.codfebyaidan.ch2;
+
+class Factorials {
+    static int computeFactorials(int k) {
+        if (k < 0) return -1;
+        else if (k == 0 || k == 1) return 1;
+        else return k*computeFactorials(k-1);
+    }
+    public static void main(String[] args) {
+        int n;
+        n = Integer.parseInt(args[0]);
+        for (int i = 1; i <= n; i++)
+            System.out.println
+                (i + " factorial is " + computeFactorials(i));
+    }
+}
