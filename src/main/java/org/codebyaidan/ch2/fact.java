9c0bef4cf5001a98c9f0a30fee5e8109518e6b8f
CodeByAidan <67598470+CodeByAidan@users.noreply.github.com>
2023-09-28 00:32:07 -0400

    Rename Factorials.java to fact.java and update the main method with a new line number.

--- src/main/java/org/codebyaidan/ch2/fact.java
+++ src/main/java/org/codebyaidan/ch2/fact.java
@@ -18,16 +18,20 @@ class Factorials {
 
 /* ............... Example compile and run(s)
 
+D:\>cd .\src\main\java\org\codebyaidan\ch2\
+
 D:\>javac fact.java
 
-D:\>java Factorials 5
+D:\>cd ../../../
+
+D:\>java -cp . org.codebyaidan.ch2.Factorials 5
 1 factorial is 1
 2 factorial is 2
 3 factorial is 6
 4 factorial is 24
 5 factorial is 120
 
-D:\>java Factorials
-java.lang.ArrayIndexOutOfBoundsException: 0
-        at Factorials.main(fact.java:11)
+D:\>java -cp . org.codebyaidan.ch2.Factorials
+Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 0 out of bounds for length 0
+        at org.codebyaidan.ch2.Factorials.main(fact.java:13)
                                             ... end of example run(s)  */
